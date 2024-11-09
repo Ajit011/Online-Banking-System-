b5655c991ff060e5e4d7243fca0f8de336d3791d
Ajit Raj <151240722+Ajit011@users.noreply.github.com>
2024-11-09 15:08:42 +0530

    Add files via upload

---
+++ Con.java
@@ -0,0 +1,18 @@
+package Online.Banking.System;
+
+import java.sql.*;
+
+public class Con {
+    Connection connection;
+    Statement statement;
+    public Con(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem", "root", "Ajit@#2005");
+           statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
